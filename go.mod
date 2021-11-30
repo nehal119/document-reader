844295029026ab03613a3abe125f078bfab1cfc3
Nehal Ahmad <nehal.ahmad@arnowa.com>
2021-11-30 19:04:22 +0530

    start

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/nehal119/document-reader
-
-go 1.16
-
-require fyne.io/fyne/v2 v2.0.3
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
+// import { MyApp } from './Pdf';
+import Sample from './Pdf2';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/nehal119/">
+        Nehal Ahmad
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Simple Document Reader
+          </Typography>
+        </Box>
+      </Container>
+    <Container maxWidth="sm">
+      <Sample />
+    </Container>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Copyright />
+      </Box>
+    </Container>
+    </>
+  );
+}
